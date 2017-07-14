6c19329be636c65a60d2bee23aff9ff77483c83b
turtles4all <jadki10@wgu.edu>
2017-07-14 14:07:11 -0400

    Changed Spacing

--- cprintf.c
+++ cprintf.c
@@ -2,22 +2,22 @@
 #include <string.h>
 #include <sys/types.h>
 int _c_printf(char *format, ... )
-{	
-	int l = strlen(format);
-	char *t_format = (char *)calloc(1, (sizeof(char) * 2)+(l * sizeof(char)));
-	char *s = "%s";
-	va_list arg;
-	int done;
-	
-	sprintf(t_format, "%s%s", format, s);
-	format = t_format;
-	
-	va_start(arg, format);
-	done = vfprintf(stdout, format, arg);
-	va_end(arg);
-	
-	free(format);
-	return done;
+{   
+    int l = strlen(format);
+    char *t_format = (char *)calloc(1, (sizeof(char) * 2)+(l * sizeof(char)));
+    char *s = "%s";
+    va_list arg;
+    int done;
+    
+    sprintf(t_format, "%s%s", format, s);
+    format = t_format;
+    
+    va_start(arg, format);
+    done = vfprintf(stdout, format, arg);
+    va_end(arg);
+    
+    free(format);
+    return done;
 }
 
 
